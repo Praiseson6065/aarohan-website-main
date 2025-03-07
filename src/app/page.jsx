ce177f1e85d071ae87e618e2ed1ba548738c576e
Praiseson <y.praiseson@outlook.com>
2025-03-07 21:33:41 +0530

    Init

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,18 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Home() {
-  const textRef = useRef(null);
-
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
     gsap.fromTo(
-      textRef.current,
+      "#textAnimate",
       { opacity: 1, scale: 0.5 },
       {
         opacity: 1,
-        scale: 2,
+        scale: 1.5,
         duration: 5,
         scrollTrigger: {
           trigger: "#heroID",
@@ -30,12 +28,16 @@ export default function Home() {
     <>
       <div
         id="heroID"
-        className='min-h-screen flex justify-center items-center py-12 md:py-24 bg-[url("/assets/MainBG.png")] bg-cover bg-center'
+        className='min-h-screen flex justify-center items-center py-48 md:py-24 bg-[url("/assets/MainBG.png")] bg-cover bg-center'
       >
-        <div className="self-end">
-          <h1 ref={textRef} className="text-4xl font-bold text-amber-600">
-            Aarohan
-          </h1>
+        <div className="self-end text-center text-amber-600" id="textAnimate">
+          <div>
+            <h1 className="text-4xl md:text-7xl font-bold ">Aarohan</h1>
+          </div>
+          <div className="text-[0.6em] flex w-full justify-between">
+            <div>March 20-23</div>
+            <div>Lords Arena</div>
+          </div>
         </div>
       </div>
       <div className="min-h-screen flex items-center py-12 "></div>
