53d0d14ba3a007e461860ef8f7588ba1c65782c2
roy-abir05 <royabirdhn@gmail.com>
2025-03-17 21:23:54 +0530

    style: styled the header

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -53,18 +53,19 @@ const events = () => {
     const [eventToggle, setEventToggle] = useState(true);
 
   return (
-    <div className="min-h-screen text-3xl">
+    <div className="min-h-screen text-2xl select-none">
       <div className="mt-10 mb-5">
-        <li className="w-[30%] max-w-[60%] h-fit mx-auto flex items-center justify-evenly border border-red-500">
-          <ul onClick={() => setDay(0)}>DAY 1</ul>
-          <ul onClick={() => setDay(1)}>DAY 2</ul>
-          <ul onClick={() => setDay(2)}>DAY 3</ul>
+        <li className="w-[30%] max-w-[60%] h-fit mx-auto flex items-center justify-evenly">
+          <ul className={`${day===0?'text-[#FF9500]':''} transition-all duration-300`} onClick={() => setDay(0)}>DAY 1</ul>
+          <ul className={`${day===1?'text-[#FF9500]':''} transition-all duration-300`} onClick={() => setDay(1)}>DAY 2</ul>
+          <ul className={`${day===2?'text-[#FF9500]':''} transition-all duration-300`} onClick={() => setDay(2)}>DAY 3</ul>
         </li>
       </div>
-      <div className="text-2xl">
-        <div className="w-[25%] max-w-1/2 h-fit mx-auto flex items-center justify-evenly border border-blue-500">
-          <div onClick={() => setEventToggle(true)}>EVENTS</div>
-          <div onClick={() => setEventToggle(false)}>WORKSHOPS</div>
+      <div className="text-base cursor-default">
+        <div className="relative w-[18%] max-w-1/2 h-fit mx-auto px-5 py-1 flex items-center justify-between border border-x-2 border-[#FF9500] rounded-4xl">
+          <span className={`absolute w-[50%] h-[calc(100%-6px)] top-[3px] left-[3px] bg-[#C07A00] border rounded-4xl ${!eventToggle? 'translate-x-[calc(100%-6px)]':''} -z-10 transition-all duration-700 ease-out`}></span>
+          <div className="w-1/2 text-center" onClick={() => setEventToggle(true)}>EVENTS</div>
+          <div className="w-1/2 text-center" onClick={() => setEventToggle(false)}>WORKSHOPS</div>
         </div>
       </div>
 
