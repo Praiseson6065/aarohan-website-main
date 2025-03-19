298f6a69679ab28006b32b239792c755fe9fa957
Praiseson <y.praiseson@outlook.com>
2025-03-19 19:46:15 +0530

    build fix

--- src/app/sponsors/page.jsx
+++ src/app/sponsors/page.jsx
@@ -2,7 +2,20 @@
 
 
 export default function Sponsors() {
-
+  return (
+    <>
+    <div
+      id="heroID"
+      className='min-h-screen flex justify-center items-center py-48 md:py-24 bg-[url("/assets/MainBG.png")] bg-cover bg-center'
+    >
+      <div className="text-center text-amber-600" id="textAnimate">
+        <div>
+          <h1 className="text-4xl md:text-7xl font-bold ">updating Soon</h1>
+        </div>
+      </div>
+    </div>
+  </>
+  )
   return (
     <>
 
