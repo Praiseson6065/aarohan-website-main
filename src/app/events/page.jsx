ffbb4e930ede532e1f9e9bf256b7d840dc19526a
Praiseson <y.praiseson@outlook.com>
2025-03-19 15:03:56 +0530

    events

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -73,9 +73,9 @@ const events = () => {
   const [eventToggle, setEventToggle] = useState(true);
 
   return (
-    <div className="w-screen min-h-screen text-base md:text-xl lg:text-2xl select-none">
-      <div className="mt-10 mb-5">
-        <li className="w-full md:w-[60%] lg:w-[35=8%] xl-[25%] h-fit mx-auto flex px-[10%] sm:px-[20%] md:px-0 items-center justify-between md:justify-evenly text-nowrap">
+    <div className="w-screen min-h-screen text-base md:text-xl lg:text-2xl select-none ">
+      <div className="mt-24 mb-5 px-24">
+        <li className=" w-full md:w-[60%] lg:w-[35=8%] xl-[25%] h-fit mx-auto flex px-[10%] sm:px-[20%] md:px-0 items-center justify-between md:justify-evenly text-nowrap">
           <ul
             className={`${
               day === 0 ? "text-[#FF9500]" : ""
@@ -103,7 +103,7 @@ const events = () => {
         </li>
       </div>
       <div className="w-full h-[36px] tracking-tighter sm:tracking-tight md:tracking-normal text-sm md:text-base cursor-default">
-        <div className="relative h-full w-[240px] sm:w-[250px] md:w-[33%] lg:w-[24%] xl:w-[20%] max-w-[350px] mx-auto flex justify-between px-5 py-1 border border-x-2 border-[#FF9500] rounded-4xl">
+        <div className=" relative h-full w-[240px] sm:w-[250px] md:w-[33%] lg:w-[24%] xl:w-[20%] max-w-[350px] mx-auto flex justify-between px-5 py-1 border border-x-2 border-[#FF9500] rounded-4xl">
           <span
             className={`absolute w-1/2 h-[calc(100%-6px)] top-[3px] left-[3px] bg-[#C07A00] border rounded-4xl ${
               !eventToggle ? "translate-x-[calc(100%-6px)]" : ""
@@ -125,7 +125,7 @@ const events = () => {
       </div>
 
       <div className="mt-16 w-screen flex items-center justify-center p-0 md:p-5 lg:p-0">
-        <ul className="w-full grid gap-x-[20px] md:grid-cols-2 xl:grid-cols-3">
+        <ul className="w-full grid gap-x-[20px] md:grid-cols-2 xl:grid-cols-2">
           {(eventToggle ? data[day].events : data[day].workshops).map(
             (event, key) => (
               <li key={key}>
