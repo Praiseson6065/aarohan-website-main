82c430be5fa8d861ca9beb7f1b95b9a600dccceb
Praiseson <y.praiseson@outlook.com>
2025-03-20 09:00:46 +0530

    env fix

--- src/app/api/sheets/route.js
+++ src/app/api/sheets/route.js
@@ -1,6 +1,6 @@
 export async function GET() {
-  const GOOGLE_SHEET_ID = process.env.GOOGLE_SHEET_ID;
-  const API_KEY = process.env.GOOGLE_API_KEY;
+  const GOOGLE_SHEET_ID = process.env.GOOGLE_SHEET_ID || "1sd1s8ot2in6vZRPpiWMliI5fiKvKV0QQ38hgpN8GNJU";
+  const API_KEY = process.env.GOOGLE_API_KEY || "AIzaSyAI9J8d8smdzrUjGmeS_hiX7cfmg0GAN4Q";
   const SHEET_NAME = "Form responses 1";
   const SHEET_URL = `https://sheets.googleapis.com/v4/spreadsheets/${GOOGLE_SHEET_ID}/values/${SHEET_NAME}?key=${API_KEY}`;
 
